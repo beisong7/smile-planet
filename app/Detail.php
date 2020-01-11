b845a41cd0ff2fd234088b412a4f6d00fe604aaf
Benjamin <beisongabc@gmail.com>
2020-01-11 14:49:59 +0100

    ascending

--- app/Detail.php
+++ app/Detail.php
@@ -27,7 +27,7 @@ class Detail extends Model
     }
 
     public function people(){
-        return People::where('related', $this->relative)->get();
+        return People::where('related', $this->relative)->orderBy('id', 'asc')->get();
 //        return $this->hasMany(People::class, 'related', 'relative');
     }
 
